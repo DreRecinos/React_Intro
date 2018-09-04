2dbeca68dd91b43b47ec67d8fedefa22669471b8
Dre Recinos <drerecinos.web@gmail.com>
2018-09-04 13:18:25 -0700

    react state/props checkpoint 2

--- react-to-do/src/App.js
+++ react-to-do/src/App.js
@@ -6,25 +6,27 @@ class App extends Component {
   constructor(props) {
      super(props);
      this.state = {
-      todos: [
-        { description: 'Walk the cat', isCompleted: true },
-        { description: 'Throw the dishes away', isCompleted: false },
-        { description: 'Buy new dishes', isCompleted: false }
-      ]
-    };
-   }
+       todos: [
+         { description: 'Walk the cat', isCompleted: true },
+         { description: 'Throw the dishes away', isCompleted: false },
+         { description: 'Buy new clothes', isCompleted: false }
+       ]
+     };
 
-   render() {
-     return (
-       <div className="App">
-         <ul>
-         { this.state.todos.map( (todo, index) =>
-          <ToDo key={ index } />
-        )}
-         </ul>
-       </div>
-     );
    }
- }
+   
+  render() {
+    return (
+      <div className="App">
+        <ul>
+          { this.state.todos.map( (todo, index) => 
+            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } />
+          )}
+        </ul>
+
+      </div>
+    );
+  }
+}
 
 export default App;
--- react-to-do/src/components/ToDo.js
+++ react-to-do/src/components/ToDo.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+
 class ToDo extends Component {
-  render() {
+	render() {
      return (
        <li>
          <input type="checkbox" checked={ this.props.isCompleted } />
