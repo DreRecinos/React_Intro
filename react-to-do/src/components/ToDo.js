d2987c8a794563a83a95fee35546d9f4d741d222
Dre Recinos <drerecinos.web@gmail.com>
2018-09-10 16:10:44 -0700

    events 9/10

--- react-to-do/src/components/ToDo.js
+++ react-to-do/src/components/ToDo.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 
 class ToDo extends Component {
 	render() {
-     return (
-       <li>
+		return (
+			<li>
          <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
          <span>{ this.props.description }</span>
        </li>
-     );
-   }
- }
+		);
+	}
+}
 
- export default ToDo;
+export default ToDo;
